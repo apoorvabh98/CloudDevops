677f34e0ed98dc70e7175bcab4029486f026e27d
Apoorva bh <51374338+apoorvabh98@users.noreply.github.com>
2026-04-26 00:17:28 +0530

    Add additional print statement for Jenkins demo

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, World!,Demo for Jenkins intergration");
+        System.out.println("Adding poll scm");
     }
 }
