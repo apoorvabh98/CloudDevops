58f505e5611e33fb7fd0096e0f74bc20d320d538
Apoorva bh <51374338+apoorvabh98@users.noreply.github.com>
2026-04-26 00:04:58 +0530

    Fix output message in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!.DEmo for Jenkins intergration");
+        System.out.println("Hello, World!,Demo for Jenkins intergration");
     }
 }
